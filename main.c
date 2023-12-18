f238fc59a8a8485c3af105df874654129f3cbfd5
Christian Harjuno <82799882+SorataBaka@users.noreply.github.com>
2023-12-18 16:35:06 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,64 @@
 #include <ncurses.h>
 #include <locale.h>
 #include "definition.h"
+#include <math.h>
+
+
+void printSprite(Position printPosition){
+    mvaddch(printPosition.y, printPosition.x, '#');
+}
+
+void initialize(int * playerAmount, Player * playerArray){
+    //Initialize start screen.
+    erase();
+    mvaddstr(1, 3, "Galactic War");
+    mvaddstr(2, 3, "Welcome to Galactic Wars!");
+    mvaddstr(4, 3, "How many players [1 - 4]: ");
+    scanw("%d", playerAmount);
+    if(*playerAmount < 2 || *playerAmount > 4){
+        erase();
+        mvaddstr(1, 3, "Invalid player size. Please enter again.");
+        mvaddstr(3, 3, "How many players [1 - 4]: ");
+    }
+
+    //Initialize the player directory.
+    playerArray = (Player *)malloc(sizeof(Player) * *playerAmount);
+
+    //Start prompting for basic information.
+    erase();
+    for(int i = 0; i < *playerAmount; i++){
+        Player * mem = playerArray + i;
+        mem->id = i;
+        mem->score = 0;
+        mem->trajectory = down;
+        mem->pos = {i + 5, i + 5};
+
+
+        mvaddstr(1, 3, "Enter username: ");
+        scanw("%s", mem->username);
+        mvaddstr(2, 3, "Key binding for turning CLOCKWISE: ");
+        scanw("%c", mem->user_bindings.clockwise);
+        mvaddstr(3, 3, "Key binding for turning ANTICLOCKWISE: ");
+        scanw("%c", mem->user_bindings.anticlockwise);
+        mvaddstr(4, 3, );
+
+
+    }
+}
 
 int main(){
+    int width, height;
+    int playerAmount;
+    Player * playerArray;
+
     initscr();
+    erase();
 
-    mvprintw(0, 0, "Hello World");
-
+    getmaxyx(stdscr, height, width);
+    Position screenMiddle = {width/2, height/2};
     timeout(-1);
     while(true){
+        initialize(&playerAmount);
         if(getch() == 'q') break;
     }
     endwin();
