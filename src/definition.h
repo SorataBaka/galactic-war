d2ef3084c25dbd9ba99fe8584b07549a4b757d67
Christian Harjuno <82799882+SorataBaka@users.noreply.github.com>
2024-01-15 10:30:18 +0900

    Add files via upload

---
+++ src/definition.h
@@ -0,0 +1,29 @@
+#ifndef DEFINITION_H
+#define DEFINITION_H
+
+#define true 1
+#define false 0
+
+typedef struct {
+    char left;
+    char right;
+    char shoot;
+} Bindings;
+
+typedef struct {
+    int x;
+    int y;
+} Position;
+
+typedef struct {
+    char username[20];
+    int health;
+    Bindings userBindings;
+    Position currentPosition;
+} Player;
+
+typedef struct {
+    Position missilePosition;
+} Missile;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include "definition.h"
+#include <ncurses.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    Bindings testBindings = {
+        'a', 'd', ' '
+    };
+    Position startingPosition = {0, 0};
+    Player testPlayer;
+    strcpy(testPlayer.username, "sora");
+    testPlayer.health = 100;
+    testPlayer.userBindings = testBindings;
+    testPlayer.currentPosition = startingPosition;   
+}
