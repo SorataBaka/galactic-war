060e04f2587bdf070e22725e759f38d7813d565e
Christian Harjuno <82799882+SorataBaka@users.noreply.github.com>
2024-01-28 15:33:25 +0000

    feat: extra information on main screen

--- src/mainscreen.c
+++ src/mainscreen.c
@@ -14,6 +14,7 @@ void mainScreen(Player * playerObject, Meteor * meteorArray, int maxHeight, int 
     while(true){
         erase();
         //Display the main title
+        mvaddstr(0, 0, "National Institute of Technology Kushiro Kosen - 3rd Year Programming Course - Christian Harjuno");
         mvaddstr((yCenter)-3, (xCenter)-44, "  _____          _               _____ _______ _____ _____  __          __     _____  \n");
         mvaddstr((yCenter)-2, (xCenter)-44, " / ____|   /\\   | |        /\\   / ____|__   __|_   _/ ____| \\ \\        / /\\   |  __ \\ \n");
         mvaddstr((yCenter)-1, (xCenter)-44, "| |  __   /  \\  | |       /  \\ | |       | |    | || |       \\ \\  /\\  / /  \\  | |__) |\n");
@@ -21,6 +22,7 @@ void mainScreen(Player * playerObject, Meteor * meteorArray, int maxHeight, int 
         mvaddstr((yCenter)+1, (xCenter)-44, "| |__| |/ ____ \\ |____ / ____ \\ |____   | |   _| || |____     \\  /\\  / ____ \\| | \\ \\ \n");
         mvaddstr((yCenter)+2, (xCenter)-44, " \\_____/_/    \\_\\______/_/    \\_\\_____|  |_|  |_____\\_____\\     \\/  \\/_/    \\_\\_|  \\_\\ \n");
 
+        mvaddstr(yCenter + 6, xCenter-21, "https://soratabaka.github.io/galactic-war/");
 
         mvaddstr(yCenter + 9, xCenter-7, "[1] Start Game");
         mvaddstr(yCenter + 10, xCenter-7, "[2] Leaderboard");
