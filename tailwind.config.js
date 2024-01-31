728c365a043a6df05b66c792c078660ce5cb853c
Christian Harjuno <harjuno.tian@gmail.com>
2024-01-31 09:31:57 +0900

    docs: changed tailwind to compilation based, added new info to page, added favicon, new images, cleaned up page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./docs/*.{html,js}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
